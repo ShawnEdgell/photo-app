e5e0d5aa0f0c6707267dd291747be4de425b2877
shawn edgell <shawn_edgell@hotmail.com>
2023-10-03 12:12:28 -0400

    added drag and drop function, can now move images around between columns

--- app.js
+++ app.js
@@ -18,3 +18,58 @@ modal.addEventListener("click", function (e) {
         modal.style.display = "none"; // Close the modal
     }
 });
+
+let draggedItem = null;
+let draggedItemParent = null; 
+
+images.forEach(function (image) {
+    
+image.addEventListener('dragstart', function (e) {
+    draggedItem = this;
+    draggedItemParent = this.parentElement;
+    
+    // Create a clone of the dragged image and adjust its size for the drag preview
+    let clone = this.cloneNode(true);
+    clone.style.width = '50px';
+    clone.style.height = '50px';
+    const dragContainer = document.getElementById('drag-container');
+    dragContainer.innerHTML = ''; // Clear previous drag previews
+    dragContainer.appendChild(clone);
+    
+    // Set the drag image to be the clone and adjust the positioning under the cursor
+    e.dataTransfer.setDragImage(clone, 25, 25);  // 25, 25 offsets center the image under the cursor
+    
+    setTimeout(() => {
+        this.style.opacity = '0.5';
+    }, 0);
+});
+    
+    // When dragging ends
+    image.addEventListener('dragend', function () {
+        this.style.opacity = '1';
+    });
+
+    // Loop through each image container div for dragover and drop events
+    image.parentElement.addEventListener('dragover', function (e) {
+        e.preventDefault();
+    });
+
+    document.addEventListener('dragover', function(e) {
+        e.preventDefault();
+    });
+    
+    // Drop event
+    image.parentElement.addEventListener('drop', function (e) {
+        e.preventDefault();
+        if (draggedItem !== this.querySelector('img')) {
+            let targetImage = this.querySelector('img');
+            draggedItemParent.appendChild(targetImage);
+            this.appendChild(draggedItem);
+        }
+        draggedItem.style.opacity = '1';
+    });
+
+});
+
+
+
